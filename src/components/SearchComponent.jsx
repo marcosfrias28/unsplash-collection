fabe43ee1237a22f7f51a9cedd6fc709fc3975f2
Marcos  Frias Molina <marcofrias0828@gmail.com>
2024-05-10 02:46:29 +0200

    chore: Update authorization header in SearchComponent to use environment variable for API key

--- src/components/SearchComponent.jsx
+++ src/components/SearchComponent.jsx
@@ -1,17 +1,14 @@
 import { useState } from 'react'
 
-let API_KEY = ''
-if (import.meta.env.DEV) {
-  API_KEY = import.meta.env.PUBLIC_UNSPLASH_ACCESS_KEY
-} else {
-  API_KEY = import.meta.env.UNSPLASH_ACCESS_KEY
-}
+let API_KEY = import.meta.env.PUBLIC_UNSPLASH_ACCESS_KEY
 
 //TODO Env variables are not working in Astro
 async function getImagesBy (name) {
   return await fetch(`https://api.unsplash.com/search/photos?query=${name}`, {
     headers: {
-      Authorization: `Client-ID ${API_KEY}`
+      Authorization: `Client-ID ${
+        import.meta.env.UNSPLASH_ACCESS_KEY || API_KEY
+      }`
     }
   })
     .then(response => response.json())
