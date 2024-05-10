a2e5a09f4f8886baecf040bbf23a30785a706f06
Marcos  Frias Molina <marcofrias0828@gmail.com>
2024-05-10 02:00:50 +0200

    chore: Update Astro configuration files and dependencies

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,15 @@
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
-
+import dotenv from "dotenv";
 import react from "@astrojs/react";
+
+dotenv.config();
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), react()]
+  integrations: [tailwind(), react()],
+  output: "hybrid",
+  define: {
+    "process.env": process.env,
+  },
 });
---
+++ src/components/SearchComponent.jsx
@@ -0,0 +1,67 @@
+import { useState } from 'react'
+
+async function getImagesBy (name) {
+  return await fetch(`https://api.unsplash.com/search/photos?query=${name}`, {
+    headers: {
+      Authorization: `Client-ID ${import.meta.env.UNSPLASH_ACCESS_KEY}`
+    }
+  })
+    .then(response => response.json())
+    .then(data => data.results)
+}
+
+export function SearchComponent (name) {
+  const [keywords, setKeywords] = useState('')
+
+  async function handleSubmit (e) {
+    e.preventDefault()
+    const results = await getImagesBy(keywords)
+    console.log('Searching for:', keywords, results)
+  }
+  return (
+    <form
+      onSubmit={handleSubmit}
+      className='min-w-xl mx-auto w-full sm:w-[550px]'
+    >
+      <label
+        htmlFor='default-search'
+        className='mb-2 text-sm font-medium sr-only dark:text-white'
+      >
+        Search
+      </label>
+      <div className='relative w-full'>
+        <input
+          onChange={e => setKeywords(e.target.value)}
+          type='search'
+          id='default-search'
+          className='block w-full p-6 pe-10 text-sm border border-gray-300 rounded-lg bg-gray-50 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500'
+          placeholder='Enter your keywords...'
+          required
+        />
+        <div className='absolute inset-y-0 end-0 mr-4 flex items-center ps-3 pointer-events-none'>
+          <svg
+            width='24'
+            height='24'
+            viewBox='0 0 24 24'
+            fill='none'
+            xmlns='http://www.w3.org/2000/svg'
+          >
+            <circle
+              cx='11'
+              cy='11'
+              r='7'
+              stroke='#E5E7EB'
+              strokeWidth='2'
+            ></circle>
+            <path
+              d='M20 20L17 17'
+              stroke='#E5E7EB'
+              strokeWidth='2'
+              strokeLinecap='round'
+            ></path>
+          </svg>
+        </div>
+      </div>
+    </form>
+  )
+}
