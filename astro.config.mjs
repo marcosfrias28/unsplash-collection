bc16d9fd896a29e34beb4e4d3e94839ced948fa4
Marcos  Frias Molina <marcofrias0828@gmail.com>
2024-05-10 02:17:00 +0200

    chore: Update Astro configuration and dependencies, add Vercel integration

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,14 +2,16 @@ import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
 import dotenv from "dotenv";
 import react from "@astrojs/react";
-
+import vercel from "@astrojs/vercel/serverless";
 dotenv.config();
+
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind(), react()],
   output: "hybrid",
   define: {
-    "process.env": process.env,
+    "process.env": process.env
   },
+  adapter: vercel()
 });
--- src/components/SearchComponent.jsx
+++ src/components/SearchComponent.jsx
@@ -1,10 +1,10 @@
 import { useState } from 'react'
-
+const API_KEY = import.meta.env.UNSPLASH_ACCESS_KEY
 //TODO Env variables are not working in Astro
 async function getImagesBy (name) {
   return await fetch(`https://api.unsplash.com/search/photos?query=${name}`, {
     headers: {
-      Authorization: `Client-ID ${import.meta.env.UNSPLASH_ACCESS_KEY}`
+      Authorization: `Client-ID ${API_KEY}`
     }
   })
     .then(response => response.json())
