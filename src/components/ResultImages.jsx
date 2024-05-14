53b6aec661bb055252bd67dbe29a9adde6183c32
Marcos  Frias Molina <marcofrias0828@gmail.com>
2024-05-14 15:10:47 +0200

    refactor: Update ResultImages component to display 12 search results instead of 8 on a Grid Container

--- src/components/ResultImages.jsx
+++ src/components/ResultImages.jsx
@@ -7,34 +7,42 @@ export function ResultImages () {
   useEffect(() => {
     async function getImagesByKeywords(keywords) {
       const response = await fetch(
-        `https://api.unsplash.com/search/photos?page=2&query=${keywords}&per_page=8`,
+        `https://api.unsplash.com/search/photos?page=2&query=${keywords}&per_page=12`,
         {
+          method: "GET",
           headers: {
             Authorization: `Client-ID ${API_KEY}`,
           },
         }
       );
       const data = await response.json();
-      setSearchResults(data.results);
+      if (data.results) setSearchResults(data.results);
+      else return;
     }
-      //getImagesByKeywords(keywords);
-      console.log(searchResults);
+    getImagesByKeywords(keywords);
   }, [keywords]);
 
 
 
   return (
-    <section className='grid grid-cols-1 sm:grid-cols-2 md:grid-cols-8 lg:grid-cols-10 auto-rows-[200px] gap-4 mx-auto p-20 max-w-[1400px]'>
-      {searchResults.map(({ id, urls, alt_description }, index) => (
-        <picture key={id} className={`w-full row-span-${index % 2 === 0 ? 1 : 2} col-span-${index % 2 === 0 ? 4 : 2}`}>
+    <section className="grid auto-rows-[192px] grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6 max-w-[1440px] mx-auto py-24 px-6">
+  {searchResults?.map(({id, urls, alt_description}, i) => (
+    <picture
+      key={id}
+      className={`hover:scale-105 transition-all ease-in row-span-1 rounded-xl bg-neutral-100 dark:bg-neutral-900 ${
+        i === 3 || i === 5 || i === 10 ? "col-span-1 lg:col-span-2" : ""
+      } ${
+        i === 2 || i === 5 || i === 10 || i === 11 ? "row-span-1 lg:row-span-2" : ""
+      }`}
+    >
         <img
           id={id}
-          src={urls.small}
+          src={urls.regular}
           alt={alt_description}
-          className='cursor-pointer object-cover w-full object-center rounded-lg shadow-xl hover:scale-110 transition-all duration-500 ease-in-out'
+          className='cursor-pointer object-cover w-full h-full rounded-xl '
         />
-      </picture>
-      ))}
-    </section>
+    </picture>
+  ))}
+</section>
   )
 }
