cdf6e808962c25630ae69346d80c2ff0859c77d9
Marcos  Frias Molina <marcofrias0828@gmail.com>
2024-05-20 18:26:35 +0200

    refactor: Update SelectedImage component to add modal for image display

--- src/components/details/SelectedImage.jsx
+++ src/components/details/SelectedImage.jsx
@@ -1,12 +1,31 @@
 import { useMediaStore } from "@/store/MediaStore";
+import { useState } from "react";
 
 function SelectedImage({ id, url, alt_description }) {
   const selectedImage = useMediaStore((state) => state.selectedImage);
   const loading = useMediaStore((state) => state.loading);
+  const [active, setActive ] = useState(true)
 
   return (
-    <a href={'image-'+id} className={url === undefined ? "pointer-events-none" : ""}>
-      <img
+    <>
+     {active && <div
+      transition:name={`image-${id}`}
+      id="imageModal"
+      class="hidden absolute w-full h-full sm:w-3/4 sm:h-3/4 mx-auto my-1/2 left-0 right-0 z-50 backdrop:blur-xl backdrop-brightness-50"
+    >
+      <SelectedImage
+        id={id}
+        url={undefined}
+        alt_description={undefined}
+        client:load
+      />
+      <button
+        id="closeModal"
+        class="absolute uppercase right-0 top-0 px-3 py-1 m-5 text-4xl font-black bg-black/20 rounded-xl"
+        >x</button>
+    </div>}
+    <img
+      onClick={() => setSelectedImage(actualImage)}
         transition:name={`image ${id}`}
         id={id}
         src={url || selectedImage?.urls.regular}
@@ -15,7 +34,7 @@ function SelectedImage({ id, url, alt_description }) {
           loading ? "blur-3xl animate-pulse" : "hover:scale-105 duration-150"
         }  w-full h-full rounded-xl cursor-pointer transition-all duration-1000`}
       />
-    </a>
+    </>
   );
 }
 
