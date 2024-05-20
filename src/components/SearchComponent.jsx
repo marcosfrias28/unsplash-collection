79d3946d52cc6973fb17b21614404d07aab11ae6
Marcos  Frias Molina <marcofrias0828@gmail.com>
2024-05-20 15:08:36 +0200

    refactor: Update SearchComponent form submission to use input value instead of event target value

--- src/components/SearchComponent.jsx
+++ src/components/SearchComponent.jsx
@@ -35,7 +35,7 @@ export function SearchComponent(props) {
           placeholder={"Enter your keywords..."}
           type="search"
           id="input-keywords"
-          className="w-full p-6 rounded-lg border-gray-300 rounded-lg bg-gray-50 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+          className="w-full p-6 rounded-lg border-gray-300 bg-gray-50 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
           required
         />
         <button className="cursor-pointer mr-4 flex items-center ps-3">
