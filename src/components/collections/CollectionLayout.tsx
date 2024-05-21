60107e3780ae937d30723375624c1a317cc4c441
Marcos  Frias Molina <marcofrias0828@gmail.com>
2024-05-21 16:19:37 +0200

    refactor: Update page layouts, add lazy loading for images, and improve link styling and size

--- src/components/collections/CollectionLayout.tsx
+++ src/components/collections/CollectionLayout.tsx
@@ -1,25 +1,41 @@
 import { useMediaStore } from "@/store/MediaStore";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 function CollectionLayout() {
-    const [currentPage, setCurrentPage] = useState<Number>(1)
+    const [currentPage, setCurrentPage] = useState<number>(1)
     const collections = useMediaStore(state => state.collections);
     const getImageCollections = useMediaStore(state => state.getImageCollections);
-    const loading = useMediaStore(state => state.loading);
     const getImages = useMediaStore(state => state.getImages);
-    const searchResults = useMediaStore(state => state.searchResults);
-    const keywords = useMediaStore(state => state.keywords);
-    const setKeywords = useMediaStore(state => state.setKeywords);
-    const selectedImage = useMediaStore(state => state.selectedImage);
-    const setSelectedImage = useMediaStore(state => state.setSelectedImage);
-    const setLoading = useMediaStore(state => state.setLoading);
+    useEffect(() => {
+      console.log(currentPage, collections);
+      getImageCollections(currentPage);
+      console.log(collections);
+    }, [currentPage])
 
-
-  
+    function handleClick(photosAPI: string) {
+      getImages(photosAPI);
+      location.href = "/search";
+    }
     return (
+      <section>
         <div className="flex flex-wrap gap-5 max-w-[1280px] w-full h-full">
-
+          {
+          collections.map((collection) => {
+            const {id, cover, description, photosAPI, title, published_at, total_photos, user  } = collection;
+            return (
+              <div onClick={()=> handleClick(photosAPI)} id={id}>
+                <img src={cover} alt={description} />
+              </div>
+            
+            )
+          })
+          }
         </div>
+        <button onClick={()=> {
+          setCurrentPage(currentPage + 1);
+          getImageCollections(currentPage);
+        }}>Load more results</button>
+      </section>
     )
 }
 
--- src/store/MediaStore.ts
+++ src/store/MediaStore.ts
@@ -5,19 +5,36 @@ import { persist, devtools } from "zustand/middleware";
 
 let API_KEY = import.meta.env.PUBLIC_UNSPLASH_API_KEY;
 
+interface collections {
+  id:string;
+  title:string;
+  description:string;
+  cover:string;
+  user: {
+    name:string;
+    profile_image:string;
+    portfolio_url:string;
+  },
+  total_photos:number;
+  published_at: string;
+  photosAPI: string;
+}
+
 interface mediaTypes {
   loading: boolean;
   keywords: string;
   defaultResults: any[];
   searchResults: any[];
-  collections: any[];
+  collections: collections[];
   selectedImage: object | null;
   setKeywords: (keywords: string) => void;
   getImageCollections: (page: number) => void;
-  getImages: () => void;
+  getImages: (photosAPI: string) => void;
   setLoading: (loading: boolean) => void;
   setSelectedImage: (selectedImage: object) => void;
 }
+
+
 
 export const useMediaStore = create<mediaTypes>()(devtools(persist((set, get) => ({
     loading: true,
@@ -28,11 +45,11 @@ export const useMediaStore = create<mediaTypes>()(devtools(persist((set, get) =>
     searchResults: [],
     getImageCollections: (page) => {
       console.log("get Collections");
-      const { keywords, setLoading } = get();
+      const { setLoading } = get();
       setLoading(true);
       axios
         .get(
-          `https://api.unsplash.com/search/photos?page=${page}&per_page=9`,
+          `https://api.unsplash.com/collections?page=${page}&per_page=12`,
           {
             method: "GET",
             headers: {
@@ -42,35 +59,34 @@ export const useMediaStore = create<mediaTypes>()(devtools(persist((set, get) =>
         )
         .then(({ data }) => {
           const result = data.results.map(
-            ({ id, urls, links, alt_description, user, created_at }) => ({
-              id: id,
-              urls: {
-                small: urls.small,
-                regular: urls.regular,
-              },
-              alt_description: alt_description || "",
+            ({ id, title, description, total_photos, published_at, user, cover_photo, links }) => ({
+              id,
+              title,
+              description,
+              cover: cover_photo.urls.regular,
               user: {
                 name: user.name,
                 profile_image: user.profile_image.large,
                 portfolio_url: user.portfolio_url,
               },
-              created_at: created_at,
-              link: links.download,
+              total_photos,
+              published_at,
+              photosAPI: links.photos,
             })
           );
-          set(state => ({ ...state, searchResults: result }));
+          set(state => ({ ...state, collections: [...state.collections, result] }));
         })
         .catch((error) => console.log(error)).finally(() => setLoading(false));
     },
     setKeywords: (keywords) => set({ keywords }, false, 'Keywords changes'),
     setSelectedImage: (selectedImage) => set({ selectedImage }, false, 'selectedImage'),
     setLoading: (loading) => set({ loading }, false, 'Loading'),
-    getImages: () => {
+    getImages: (photosAPI) => {
       console.log("get Images");
       const { keywords, setLoading } = get();
+      const url = photosAPI ? photosAPI : `https://api.unsplash.com/search/photos?page=1&query=${keywords}&per_page=12`;
       axios
-        .get(
-          `https://api.unsplash.com/search/photos?page=2&query=${keywords}&per_page=12`,
+        .get(url,
           {
             method: "GET",
             headers: {
