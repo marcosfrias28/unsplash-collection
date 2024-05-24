832303bb5f84539984c12ed3f611cdfdeec8dfc8
Marcos  Frias Molina <marcofrias0828@gmail.com>
2024-05-24 15:41:33 +0200

    refactor: Update CollectionLayout to use useCollectionStore instead of useMediaStore

--- src/components/collections/CollectionLayout.tsx
+++ src/components/collections/CollectionLayout.tsx
@@ -1,12 +1,13 @@
 import { useCollectionStore, useMediaStore } from "@/store/MediaStore";
-import Image from "astro/components/Image.astro";
 import moment from "moment";
 import { useEffect, useState } from "react";
+
 function CollectionLayout() {
   const [currentPage, setCurrentPage] = useState<number>(1)
   const collections = useCollectionStore(state => state.collections);
   const getImageCollections = useCollectionStore(state => state.getImageCollections);
   const getImages = useMediaStore(state => state.getImages);
+  
   useEffect(() => {
     getImageCollections(currentPage);
   }, [currentPage])
@@ -24,14 +25,11 @@ function CollectionLayout() {
           return (
             <article key={id} className="mb-4">
               <div className="group w-96 h-96 relative" onClick={() => handleClick(photosAPI, title)} id={id}>
-                <Image width='100' height='100' class="rounded-md object-cover object-center h-full w-full" src={cover} alt={description || title || "No description found"} />
+                <img className="rounded-md object-cover object-center h-full w-full" src={cover} alt={description || title || "No description "} />
               </div>
               <div className="flex flex-col gap-1 mt-3">
                 <p className="text-black dark:text-white font-bold text-lg">{title}</p>
                 <small className="text-sm font-medium">{total_photos} photos</small>
-                {/* <small className="text-sm mb-5 font-extralight">
-                  Published on {moment(published_at).utc().format("LL")}
-                </small> */}
               </div>
             </article>
           )
