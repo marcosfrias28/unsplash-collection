de9c0fc84b5c9511313acdbec093622cc019dad3
Marcos  Frias Molina <marcofrias0828@gmail.com>
2024-05-26 01:38:43 +0200

    Random Render of Collections every render

--- src/components/collections/CollectionLayout.tsx
+++ src/components/collections/CollectionLayout.tsx
@@ -10,10 +10,9 @@ function getRandomPage() {
 
 
 function CollectionLayout() {
-  const [initialRender, setInitialRender] = useState(true)
   const [loading, setLoading] = useState(false)
   const [currentPage, setCurrentPage] = useState<number>(getRandomPage())
-  const getImages = useMediaStore(state => state.getImages);
+  const getCollectionImages = useMediaStore(state => state.getCollectionImages);
   const [collections, setCollections] = useState<collection[]>([]);
 
   const getCollections = useCallback(() => {
@@ -49,29 +48,27 @@ function CollectionLayout() {
 
   useEffect(() => {
     getCollections();
-    console.log(collections);
   }, [currentPage])
 
   function handleClick(photosAPI: string, title: string, total_photos: number) {
-    getImages(photosAPI);
+    getCollectionImages(photosAPI);
     location.href = `/collection/${title}?total_photos=${total_photos}`;
   }
   function handleScroll() {
     setCurrentPage(current => current + 1)
-    getCollections()
   }
 
   return (
-    <section className="flex flex-col justify-center items-center ">
-      <div className="flex flex-wrap gap-8 max-w-[1280px] w-full h-full my-10">
-        {collections.length > 0 && collections.map((collection) => {
+    <>
+      <div className="flex flex-wrap gap-8 max-w-[1280px] w-full h-full my-10 justify-center items-center">
+        {collections && collections.map((collection) => {
           const { id, cover, description, photosAPI, title, total_photos } = collection;
           return (
             <article key={id} className="mb-4">
               <div className="group w-96 h-96 relative" onClick={() => handleClick(photosAPI, title, total_photos)} id={id}>
                 <img className="rounded-md object-cover object-center h-full w-full" src={cover} alt={description || title || "No description "} />
               </div>
-              <div className="flex flex-col gap-1 mt-3">
+              <div className="flex flex-col gap-1 mt-3 overflow-clip w-96">
                 <p className="text-black dark:text-white font-bold text-lg">{title}</p>
                 <small className="text-sm font-medium">{total_photos} photos</small>
               </div>
@@ -80,12 +77,11 @@ function CollectionLayout() {
         })
         }
       </div>
-      <div className="flex justify-center items-center">
-        {loading && <div className="animate-spin rounded-full h-32 w-32 border-t-2 border-b-2 border-gray-900"></div>}
-      </div>
-      <button onClick={handleScroll} className='rounded-3xl px-2 py-3 hover:scale-105 transition-all bg-gradient-to-r from-[rgba(235,199,156,1)] via-[rgba(223,149,168,1)] to-[rgba(128,57,126,1)]' ><span className="bg-white text-black px-5 py-2 rounded-3xl font-bold">Load more results</span></button>
-    </section >
-
+      {loading && <div className="flex justify-center items-center my-20">
+        <div className="animate-spin rounded-full h-32 w-32 border-t-2 border-b-2 border-gray-900 dark:border-white"></div>
+      </div>}
+      <button onClick={handleScroll} className='rounded-3xl px-2 py-3 hover:scale-105 transition-all bg-gradient-to-r from-[rgba(235,199,156,1)] via-[rgba(223,149,168,1)] to-[rgba(128,57,126,1)] w-fit mx-auto' ><span className="bg-white text-black px-5 py-2 rounded-3xl font-bold">Load more results</span></button>
+    </>
   )
 }
 
