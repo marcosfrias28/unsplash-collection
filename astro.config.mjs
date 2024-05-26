25b03096754adf95c01b0cd8a4abed2d49ac3039
Marcos  Frias Molina <marcofrias0828@gmail.com>
2024-05-26 04:38:02 +0200

    Update Astro config to remove unused integration

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,13 +3,12 @@ import tailwind from "@astrojs/tailwind";
 import dotenv from "dotenv";
 import react from "@astrojs/react";
 import vercel from "@astrojs/vercel/serverless";
-import db from "@astrojs/db";
 dotenv.config();
 
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), react(), db()],
+  integrations: [tailwind(), react()],
   output: "server",
   adapter: vercel()
 });
