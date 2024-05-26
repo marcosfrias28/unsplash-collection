b4bdded8bd9b58b777766bfa92dd3284ba793365
Marcos  Frias Molina <marcofrias0828@gmail.com>
2024-05-26 04:41:20 +0200

    refactor: Remove unused code and comments in db/config.ts and db/seed.ts

--- db/seed.ts
+++ db/seed.ts
@@ -1,13 +1,13 @@
-import { Users, db } from 'astro:db';
+// import { Users, db } from 'astro:db';
 
-// https://astro.build/db/seed
-export default async function seed() {
- await db.insert(Users).values([
-	{
-	id: 1,
-	username:'admin',
-	password:'123',
-	mail:'marcofrias0828@gmail.com'
- 	}
-])
-}
+// // https://astro.build/db/seed
+// export default async function seed() {
+//  await db.insert(Users).values([
+// 	{
+// 	id: 1,
+// 	username:'admin',
+// 	password:'123',
+// 	mail:'marcofrias0828@gmail.com'
+//  	}
+// ])
+// }
