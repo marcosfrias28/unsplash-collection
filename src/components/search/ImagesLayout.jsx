595e2c77776653fd69e2e966f563e520e2131f7f
Marcos  Frias Molina <marcofrias0828@gmail.com>
2024-05-26 16:50:53 +0200

    Updated Details Path, moved to /details/image-

--- src/components/search/ImagesLayout.jsx
+++ src/components/search/ImagesLayout.jsx
@@ -1,7 +1,7 @@
 import { useMediaStore } from "@/store/MediaStore";
 import SelectedImage from "../details/SelectedImage";
 
-export function ImagesLayout({layout}) {
+export function ImagesLayout({ layout }) {
   const defaultResults = useMediaStore((state) => state.defaultResults);
   const searchResults = useMediaStore((state) => state.searchResults);
 
@@ -17,22 +17,20 @@ export function ImagesLayout({layout}) {
           <picture
             onClick={() => setSelectedImage(actualImage)}
             key={id}
-            style={{viewTransitionName: `image-${id}`}}
-            className={`hover:shadow-md drop-shadow-xl overflow-hidden hover:shadow-zinc-900 transition-all ease-in row-span-1 rounded-xl bg-neutral-700 ${
-              i === 3 || i === 5 || i === 10 ? "col-span-1 lg:col-span-2" : ""
-            } ${
-              i === 2 || i === 5 || i === 10 || i === 11
+            style={{ viewTransitionName: `image-${id}` }}
+            className={`hover:shadow-md drop-shadow-xl overflow-hidden hover:shadow-zinc-900 transition-all ease-in row-span-1 rounded-xl bg-neutral-700 ${i === 3 || i === 5 || i === 10 ? "col-span-1 lg:col-span-2" : ""
+              } ${i === 2 || i === 5 || i === 10 || i === 11
                 ? "row-span-2 sm:row-span-3 lg:row-span-4"
                 : "row-span-2"
-            }`}
+              }`}
           >
-         <a href={'image-'+id}>
-            <SelectedImage
-              id={id}
-              url={urls.small}
-              alt_description={alt_description}
-            />
-          </a>
+            <a href={'/details/image-' + id}>
+              <SelectedImage
+                id={id}
+                url={urls.small}
+                alt_description={alt_description}
+              />
+            </a>
           </picture>
         );
       })}
