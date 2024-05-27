b77ae349fd55d1fae8d7bab6fc65a8b44edbd545
Marcos  Frias Molina <marcofrias0828@gmail.com>
2024-05-27 14:31:48 +0200

    SelectedImage component to fix image source and alt attribute

--- src/components/details/SelectedImage.jsx
+++ src/components/details/SelectedImage.jsx
@@ -4,9 +4,8 @@ function SelectedImage({ id, url, alt_description, className }) {
   const selectedImage = useMediaStore((state) => state.selectedImage);
   const loading = useMediaStore((state) => state.loading);
   return (
-    <img id={id} src={url || selectedImage?.urls.regular} alt={alt_description || selectedImage?.alt_description} className={`object-cover ${
-          loading ? "blur-3xl animate-pulse" : "hover:scale-105 duration-150"
-        }  w-full h-full rounded-xl cursor-pointer transition-all duration-1000 ${className}`}></img>
+    <img id={id} src={url || selectedImage?.urls?.regular} alt={alt_description || selectedImage?.altDescription} className={`object-cover ${loading ? "blur-3xl animate-pulse" : "hover:scale-105 duration-150"
+      }  w-full h-full rounded-xl cursor-pointer transition-all duration-1000 ${className}`}></img>
   );
 }
 
