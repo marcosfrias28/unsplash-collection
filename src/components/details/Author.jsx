8a734e9e6c6fe476228faa1115f34420a4873eb7
Marcos  Frias Molina <marcofrias0828@gmail.com>
2024-05-27 16:40:14 +0200

    refactor: Update Author component to use optional chaining for user properties

--- src/components/details/Author.jsx
+++ src/components/details/Author.jsx
@@ -5,14 +5,14 @@ export function AuthorProfile() {
   const selectedImage = useMediaStore((state) => state.selectedImage);
   console.log(selectedImage?.user);
   return (
-    <picture class="flex flex-row gap-4 items-center">
+    <picture className="flex flex-row gap-4 items-center">
 
       <img
         className="w-12 h-12 rounded-full bg-slate-100"
-        src={selectedImage?.user?.profileImage?.large}
+        src={selectedImage?.user?.profile_image?.large}
         alt="Photo profile of the Author / Owner of this photo"
       />
-      <a href={selectedImage?.user?.portfolioURL || 'https://unsplash.com/'} target="_blank" className="font-bold text-black dark:text-white text-lg hover:underline">
+      <a href={selectedImage?.user?.portfolio_url || 'https://unsplash.com/'} target="_blank" className="font-bold text-black dark:text-white text-lg hover:underline">
         {selectedImage?.user?.name}
       </a>
 
@@ -26,7 +26,7 @@ export function PublishedDate(props) {
   return (
     <>
       <small {...props} className="text-sm mb-5">
-        Published on {moment(selectedImage?.createdAt).utc().format("LL")}
+        Published on {moment(selectedImage?.created_at).utc().format("LL")}
       </small>
     </>
   );
