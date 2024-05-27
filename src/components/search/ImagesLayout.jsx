6ca6a51a671a665e70e46556ee4c54f084c15199
Marcos  Frias Molina <marcofrias0828@gmail.com>
2024-05-27 16:41:39 +0200

    refactor: Optimize ImagesLayout component and update setSelectedImage function

--- src/components/search/ImagesLayout.jsx
+++ src/components/search/ImagesLayout.jsx
@@ -4,9 +4,7 @@ import SelectedImage from "../details/SelectedImage";
 export function ImagesLayout({ layout }) {
   const defaultResults = useMediaStore((state) => state.defaultResults);
   const searchResults = useMediaStore((state) => state.searchResults);
-
-  const setSelectedImage = useMediaStore((state) => state.setSelectedImage);
-
+  const setSelectedImage = useMediaStore(state => state.setSelectedImage)
   const gallery = searchResults.length >= 1 ? searchResults : defaultResults;
 
   return (
@@ -18,7 +16,7 @@ export function ImagesLayout({ layout }) {
             onClick={() => setSelectedImage(actualImage)}
             key={id}
             style={{ viewTransitionName: `image-${id}` }}
-            className={`hover:shadow-md drop-shadow-xl overflow-hidden hover:shadow-zinc-900 transition-all ease-in row-span-1 rounded-xl bg-neutral-700 ${i === 3 || i === 5 || i === 10 ? "col-span-1 lg:col-span-2" : ""
+            className={`cursor-pointer hover:shadow-md drop-shadow-xl overflow-hidden hover:shadow-zinc-900 transition-all ease-in row-span-1 rounded-xl bg-neutral-700 ${i === 3 || i === 5 || i === 10 ? "col-span-1 lg:col-span-2" : ""
               } ${i === 2 || i === 5 || i === 10 || i === 11
                 ? "row-span-2 sm:row-span-3 lg:row-span-4"
                 : "row-span-2"
