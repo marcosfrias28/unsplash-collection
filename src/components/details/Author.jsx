ec94bb40173231ecd29d0b908d4d3ff0116beeef
Marcos  Frias Molina <marcofrias0828@gmail.com>
2024-05-28 15:32:23 +0200

    At this point I'll put this project on my personal portfolio

--- src/components/details/Author.jsx
+++ src/components/details/Author.jsx
@@ -4,29 +4,21 @@ import moment from "moment";
 export function AuthorProfile() {
   const selectedImage = useMediaStore((state) => state.selectedImage);
   return (
-    <picture className="flex flex-row gap-4 items-center">
-
-      <img
-        className="w-12 h-12 rounded-full bg-slate-100"
-        src={selectedImage?.user?.profileImage?.large}
-        alt="profile of the Author"
-      />
-      <a href={selectedImage?.user?.portfolio_url || 'https://unsplash.com/'} target="_blank" className="font-bold text-black dark:text-white text-lg hover:underline">
-        {selectedImage?.user?.name}
-      </a>
-
-    </picture>
-  );
-}
-
-export function PublishedDate(props) {
-  const selectedImage = useMediaStore((state) => state.selectedImage);
-
-  return (
     <>
-      <small {...props} className="text-sm mb-5">
+      <div className="flex flex-row gap-4 items-center">
+        <img
+          className="w-12 h-12 rounded-full bg-slate-100"
+          src={selectedImage?.user?.profile_image?.medium}
+          alt={selectedImage?.alt_description}
+        />
+        <a href={selectedImage?.user?.portfolio_url || 'https://unsplash.com/'} target="_blank" className="font-bold text-black dark:text-white text-lg hover:underline">
+          {selectedImage?.user?.name}
+        </a>
+      </div>
+      <small className="text-sm mb-5">
         Published on {moment(selectedImage?.created_at).utc().format("LL")}
       </small>
     </>
+
   );
 }
